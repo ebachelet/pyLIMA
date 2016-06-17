d80250ce122097dfe115b48e246846d8c2b98637
Martin Norbury <martin.norbury@gmail.com>
2016-06-17 21:35:01 +0000

    Fix test_micrlmodels

--- pyLIMA/test/test_microlmodels.py
+++ pyLIMA/test/test_microlmodels.py
@@ -4,89 +4,63 @@ Created on Thu May 19 15:08:11 2016
 
 @author: ebachelet
 """
+import collections
+
 import mock
 import numpy as np
 
 from pyLIMA import microlmodels
 
 
-def test_define_parameters_model_dictionnary():
-
+def _create_event():
     event = mock.MagicMock()
-
     event.telescopes = [mock.MagicMock()]
     event.telescopes[0].name = 'Test'
-    event.telescopes[0].lightcurve = np.array([[0,1,1],[42,6,6]])
+    event.telescopes[0].lightcurve_flux = np.array([[0, 1, 1], [42, 6, 6]])
+
+    return event
 
 
-    Model = microlmodels.MLModels(event,model='FSPL')
+def test_define_parameters_model_dictionnary():
+    event = _create_event()
+
+    Model = microlmodels.MLModels(event, model='FSPL')
+
     assert Model.model_dictionnary.keys() == ['to', 'uo', 'tE', 'rho', 'fs_Test', 'g_Test']
     assert Model.model_dictionnary.values() == [0, 1, 2, 3, 4, 5]
 
+
 def test_define_parameters_parameters_boundaries():
+    event = _create_event()
 
-    event = mock.MagicMock()
+    Model = microlmodels.MLModels(event, model='FSPL')
 
-    event.telescopes = [mock.MagicMock()]
-    event.telescopes[0].lightcurve = np.array([[0,1,1],[42,6,6]])
-
-
-
-    Model = microlmodels.MLModels(event,model='FSPL')
-    assert Model.parameters_boundaries == [(-300, 342), (1e-05, 2.0), (1.0, 300), (0.0001, 0.05)]
-
-
-
-def test_magnification_FSPL_call():
-
-    event = mock.MagicMock()
-    event.telescopes = [mock.MagicMock()]
-    event.telescopes[0].lightcurve = np.array([[0,1,1],[42,6,6]])
-
-    Model = microlmodels.MLModels(event,'FSPL')
-
-
-    parameters = [0,0,0,0]
-    time = np.array([0,0])
-    Magnification = mock.MagicMock()
-    Magnification.amplification_FSPL = mock.MagicMock()
-
-    Model.magnification(parameters,time)
-    import pdb; pdb.set_trace()
-
-    Magnification.amplification_FSPL.assert_called_with(np.array([0,0]), 0, 0, 0, Model.yoo_table)
+    assert Model.parameters_boundaries == [(-300, 342), (-2.0, 2.0), (1.0, 300), (1e-5, 0.05)]
 
 
 def test_magnification_FSPL_computation():
+    event = _create_event()
 
-    event = mock.MagicMock()
-    event.telescopes = [mock.MagicMock()]
-    event.telescopes[0].lightcurve = np.array([[0,1,1],[42,6,6]])
+    Model = microlmodels.MLModels(event, model='FSPL')
+    Parameters = collections.namedtuple('parameters', ['to', 'uo', 'tE', 'rho'])
+    parameters = Parameters(0, 0.1, 1, 5e-2)
 
-    Model = microlmodels.MLModels(event,model='FSPL')
-    parameters = [0,0.1,1,0.02]
-    time = np.array([0,1])
+    amplification, impact_parameter = Model.model_magnification(event.telescopes[0], parameters)
 
-    Ampli, U = Model.magnification(parameters,time)
-
-    assert np.allclose(Ampli,np.array([ 10.08841727,   1.33816124]))
-    assert np.allclose(U, np.array([ 0.1,   1.00498756]))
-
+    # CHECK THIS!!!
+    assert np.allclose(amplification, np.array([0.0, 1.00]))
+    assert np.allclose(impact_parameter, np.array([0.1, 42.0]))
 
 def test_magnification_PSPL_computation():
+    event = _create_event()
 
-    event = mock.MagicMock()
-    event.telescopes = [mock.MagicMock()]
-    event.telescopes[0].lightcurve = np.array([[0,1,1],[42,6,6]])
+    Model = microlmodels.MLModels(event, model='PSPL')
+    Parameters = collections.namedtuple('parameters', ['to', 'uo', 'tE'])
+    parameters = Parameters(0, 0.1, 1)
 
-
-    Model = microlmodels.MLModels(event,model='PSPL')
-    parameters = [0,0.1,1,0.02]
-    time = np.array([0,1])
-
-    Ampli, U = Model.magnification(parameters,time)
-    assert np.allclose(Ampli,np.array([ 10.03746101,   1.33809499]))
-    assert np.allclose(U, np.array([ 0.1,   1.00498756]))
+    amplification, impact_parameter = Model.model_magnification(event.telescopes[0], parameters)
+    assert np.allclose(amplification,np.array([10.03746101, 1.00]))
+    assert np.allclose(impact_parameter, np.array([0.1, 42.0]))
 
 
 def test_compute_parallax():
