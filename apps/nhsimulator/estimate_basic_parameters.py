2ae997e50afad7f12af7bd93124b8dfaacc2d784
rachel3834 <rstreet@lco.global>
2019-02-27 17:02:44 -0800

    Updated with einstein radius function

--- apps/nhsimulator/estimate_basic_parameters.py
+++ apps/nhsimulator/estimate_basic_parameters.py
@@ -12,12 +12,21 @@ import numpy as np
 ML = 1.0 * constants.M_sun
 DL = 4e3 * constants.pc
 DS = 8e3 * constants.pc
+vT_min = 75e3 * (u.m/u.s)
+vT_max = 120e3 * (u.m/u.s)
 
 def calc_drel(DL,DS):
     return 1.0 / ( (1.0/DL) - (1.0/DS) )
 
 def calc_proj_re(ML,Drel):
     return np.sqrt( (4.0 * constants.G * ML * Drel) / (constants.c*constants.c) )
+
+def calc_RE(ML,DL,DS):
+    
+    DLS = DS-DL
+    D = (DL*DLS)/DS
+    
+    return np.sqrt((4.0*constants.G*ML*D)/(constants.c*constants.c))
 
 Drel = calc_drel(DL,DS)
 proj_re = calc_proj_re(ML,Drel)
