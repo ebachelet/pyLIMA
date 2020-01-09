3c481b2efda0248e89f519940961f112c007d7af
ebachelet <etibachelet@gmail.com>
2020-01-09 01:05:02 +0000

    Fix .travils.yml before pip*2

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
     download_url = 'https://github.com/ebachelet/pyLIMA/archive/0.1.tar.gz',
     packages=find_packages('.'),
     include_package_data=True,
-    install_requires=['scipy','numpy','matplotlib','astropy','emcee','numba','bokeh'],
+    install_requires=['scipy','numpy','matplotlib','astropy','emcee','numba','bokeh','PyAstronomy'],
+dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0'],
     python_requires='>=3.6,<4',
     test_suite="nose.collector",
     classifiers=[
