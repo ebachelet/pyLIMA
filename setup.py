49afdb8b7fa3b7f7d3979412d69dd3bf615a37ea
ebachelet <etibachelet@gmail.com>
2020-01-09 01:09:16 +0000

    Fix .travils.yml before pip*3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages('.'),
     include_package_data=True,
     install_requires=['scipy','numpy','matplotlib','astropy','emcee','numba','bokeh','PyAstronomy'],
-dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0'],
+dependency_links=['https://github.com/valboz/VBBinaryLensing/tarball/master'],
     python_requires='>=3.6,<4',
     test_suite="nose.collector",
     classifiers=[
