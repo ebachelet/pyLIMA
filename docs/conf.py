da6c08333c8023af456259d55d9bab85f3ac5c7d
Etienne Bachelet <etibachelet@gmail.com>
2020-11-18 22:14:51 +0100

    NewDoc adding readthedocs template**4

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,6 @@
+
+
+
 # -*- coding: utf-8 -*-
 #
 # pyLIMA documentation build configuration file, created by
@@ -18,7 +21,7 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../..'))
+
 
 # -- General configuration ------------------------------------------------
 
