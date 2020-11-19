196cc043ddfe5dda3c218672316356730346aa56
Etienne Bachelet <etibachelet@gmail.com>
2020-11-19 18:59:23 +0100

    Adding pyLIMA logo**2

--- docs/conf.py
+++ docs/conf.py
@@ -90,14 +90,13 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-h
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
-html_static_path = ["_static"]
+html_static_path = ["./source/_static"]
 html_logo = "_static/pyLIMA_logo.png"
 html_theme_options = {"logo_only": True}
 
