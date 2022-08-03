713979cca960d26391d7e14b53ebb6b863682041
huluvu38@gmail.com <huluvu38@gmail.com>
2022-08-03 15:54:11 +0200

    Updated examples for new version.

--- examples/pyLIMA_example_3b_new.py
+++ examples/pyLIMA_example_3b_new.py
@@ -36,9 +36,14 @@ CTIO_I = simulator.simulate_a_telescope('CTIO_I', your_event, 2458365.5, 2458965
 GAIA_G = simulator.simulate_a_telescope('GAIA_G', your_event, 2458365.5, 2458965.5, 168, 'Space', 'G',
                                         uniform_sampling=True, spacecraft_name='Gaia')
 
+SPITZER_H = simulator.simulate_a_telescope('SPITZER_H', your_event, 2458565.5, 2458765.5, 168, 'Space', 'H',
+                                        uniform_sampling=True, spacecraft_name='Spitzer')
+
+
 ### Similar to tutorial 1, we need to associate this telescopee with the event we created:
 your_event.telescopes.append(CTIO_I)
 your_event.telescopes.append(GAIA_G)
+your_event.telescopes.append(SPITZER_H)
 
 ### Run a quick sanity check on your input.
 your_event.check_event()
@@ -50,16 +55,21 @@ your_event.check_event()
 ### to be close to t0.
 from pyLIMA.models import PSPL_model
 pspl = PSPL_model.PSPLmodel(your_event, parallax=['Full', 2458565.5])
-#pspl.define_pyLIMA_standard_parameters()
 
 ### Now that the MODEL is there, we need to set the relevant parameters.
 ### pspl_parameters = [to, uo, tE, flux_source, flux_blend]
 pspl_parameters = simulator.simulate_microlensing_model_parameters(pspl)
-print (pspl_parameters)
 
+### For the purposes of this demonstration we will set the parameters to some reasonable values.
 ### Let's fix t0 to the value we set when we were preparing our MODEL (including parallax) above.
 ### This is the reference date (t0_\bar) we want to evaluate the parallax from:
 pspl_parameters[0] = 2458565.25
+pspl_parameters[1:5] = [0.60, 81.50, 0.02, 0.07]
+
+### We also artificially set the source and blend fluxes of all telescopes to the same values
+### just for plotting purposes (you don't have to do this normally):
+pspl_parameters[5::2] = [10000.,10000.,10000.] # source fluxes
+pspl_parameters[6::2] = [1000.,1000.,1000.] # blend fluxes
 
 ### Transform the parameters into a pyLIMA class object. See the documentation for details.
 pyLIMA_parameters_1 = pspl.compute_pyLIMA_parameters(pspl_parameters)
@@ -70,6 +80,9 @@ simulator.simulate_lightcurve_flux(pspl, pyLIMA_parameters_1)
 
 #### Let's plot our simulated light curve using the pyLIMA plotter:
 from pyLIMA.outputs import pyLIMA_plots
+pyLIMA_plots.plot_lightcurves(pspl, pspl_parameters)
+
+### Let's look at the source trajectory looks like from Earth and Space:
 pyLIMA_plots.plot_geometry(pspl, pspl_parameters)
 plt.show()
 
