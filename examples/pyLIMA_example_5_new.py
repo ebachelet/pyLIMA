8ec51e3499ce4426e1ea737237f8ed4e554735e5
huluvu38@gmail.com <huluvu38@gmail.com>
2023-01-11 18:37:44 +0100

    Updated examples 1 to 5 (inc. jupyterlab) for Rebranding and updates requirements.txt

--- examples/pyLIMA_example_5_new.py
+++ examples/pyLIMA_example_5_new.py
@@ -21,12 +21,13 @@ from pyLIMA.outputs import pyLIMA_plots
 your_event = event.Event()
 your_event.name = 'OB150966'
 
-#Here RA and DEC matter !!! 
+#Here RA and DEC matter (because the event has a strong parallax signal) !!! 
 your_event.ra = 268.75425
 your_event.dec = -29.047111111111114
 
 ### You now need to associate all data sets with this EVENT. 
-### There are 11 sets of observations and we need to load all of them.
+### There are 11 sets of observations and we want to include all of them. 
+### You could do this in a loop or load each of them individually as in this example.
 
 ### The data sets are already pre-formatted: 
 ###     column 1 is the date, column 2 the magnitude and column 3 
@@ -51,6 +52,10 @@ telescope_3 = telescopes.Telescope(name = 'SPITZER',
                                    light_curve = data_3.astype(float),
                                    light_curve_names = ['time','mag','err_mag'],
                                    light_curve_units = ['JD','mag','mag'])
+
+### IMPORTANT: Tell the code that SPITZER is in space!
+telescope_3.location = 'Space'
+telescope_3.spacecraft_name = 'Spitzer'
 
 data_4 = np.loadtxt('./data/DANISH_OB150966.dat')
 telescope_4 = telescopes.Telescope(name = 'DANISH', 
@@ -109,7 +114,7 @@ telescope_11 = telescopes.Telescope(name = 'LCO_SSO',
                                    light_curve_names = ['time','mag','err_mag'],
                                    light_curve_units = ['JD','mag','mag'])
 
-### Add the telescopes to your event:
+### Add the telescopes to your EVENT:
 your_event.telescopes.append(telescope_1)
 your_event.telescopes.append(telescope_2)
 your_event.telescopes.append(telescope_3)
@@ -130,118 +135,120 @@ your_event.find_survey('OGLE')
 ### Run a quick sanity check on your input.
 your_event.check_event()
 
-### Let's look at the data first to see what the light curve looks like.
-## This should give us a hint as to which model we want to explore first.
-for tel in your_event.telescopes:
-    tel.plot_data()
+### You can now quickly browse some of the light curves to look for any obvious features.
+### This should give you a hint as to which model you might want to explore first.
 
+### Invert the y-axis of the plots so that light curves are displayed the correct way up.
 plt.gca().invert_yaxis()
+
+### Loop over the telescopes and select the ones you want to plot. Here we only display 
+### the OGLE data:
+for tel in your_event.telescopes:
+    if tel.name == 'OGLE' :
+        tel.plot_data()
+
+plt.show() 
+
+### The event is highly magnified but there seem to be no obvious strong 
+### secondary features. You can try fitting it with a simple point-source, point-lens 
+### (PSPL) model.
+
+### Set up the PSPL MODEL you want to fit and link it to the EVENT. 
+from pyLIMA.models import PSPL_model
+pspl = PSPL_model.PSPLmodel(your_event)
+
+### Next you need to specify the fitting algorithm you want to use 
+### e.g. [LM_fit, TRF_fit, DE_fit, MCMC_fit, etc]. Consult the documentation for 
+### details on what each algorithm does. Let us try out a TRF fit and give it some 
+### starting guess parameters for t0, u0 and tE. You can guess roughly what starting values
+### you can try for these parameters by looking at the light curve again.
+from pyLIMA.fits import TRF_fit
+fit_1 = TRF_fit.TRFfit(pspl)
+fit_1.model_parameters_guess = [2457205.5, 1.0, 100.0]
+
+### Fit the model:
+fit_1.fit()
+
+### TRF doesn't explore the whole parameters space but it is good at narrowing in at a local miniumum. 
+
+### Let's plot it and look at the fit. Now all data will be aligned and displayed (with the exception of Spitzer, which is in space).
+pyLIMA_plots.plot_lightcurves(pspl, fit_1.fit_results['best_model'])
 plt.show()
 
-### Next, construct the MODEL you want to fit and link it to the EVENT you prepared. 
-### Since this is obviously a binary, let's go with a USBL, without second order effects:
-from pyLIMA.models import USBL_model
-fspl = USBL_model.USBLmodel(your_event)
+### The fit looks reasonable, but Zoom closely around the peak and you will notice a secondary 
+### peak. The event is a binary! There are no clear caustic-crossing features in the light curve 
+### and the duration of the secondary peak is very short (less than a day), so this could be 
+### a planet. Since you have included Spitzer observations from space, you also now need to 
+### consider parallax in your model. All this implies that the next model we should allow 
+### for all these effects in our next model.
 
-### Import the DE algorithm
+### Set up a new uniform-source, binary-lens (USBL) model and link it to the EVENT.
+### For the USBL model, we will also need to specify four extra parameters: rho, s, q and alpha,
+### as well as two more describing the parallax vector, piEN,piEE.
+### In order, the USBL parameters to be fitted are (for fancy_parameters=True): 
+### {'to': 0, 'uo': 1, 'log(tE)': 2, 'log(rho)': 3, 'log(s)': 4, 'log(q)': 5, 'alpha': 6} + [piEN,piEE]
+### (note that are also secondary parameters to be optimized that allow for data offsets 
+### and blending)
+from pyLIMA.models import USBL_model
+usbl = USBL_model.USBLmodel(your_event, parallax=['Full', 2457205.5])
+### t0par = 2457265.5
+
+### Note: When you fit for parallax (and/or orbital motion), you also need to provide a 
+### reference time, t0par, from which to perform the computations. Good choices for t0par 
+### are times close to t0, or close to points of caustic entry.
+
+### Specify the fitting algorithm. This time go for a differential evolution search of 
+### the parameter space. 
 from pyLIMA.fits import DE_fit
 
-### Enable multithreading
+fit_2 = DE_fit.DEfit(usbl, fancy_parameters=True, telescopes_fluxes_method='polyfit', DE_population_size=10, max_iteration=10000, display_progress=True)
+
+### You do not need to specify an initial position, but you do need to 
+### provide allowed ranges for each parameter:
+fit_2.fit_parameters['t0'][1] = [2457195.00, 2457215.00] # t0 limits
+fit_2.fit_parameters['u0'][1] = [0.001, 0.2] # u0 limits
+fit_2.fit_parameters['log_tE'][1] = [1.6, 2.0] # logtE limits in days
+fit_2.fit_parameters['log_rho'][1] = [-3.3, -1.3] # logrho
+fit_2.fit_parameters['log_separation'][1] = [0.0, 0.5] # logs limits
+fit_2.fit_parameters['log_mass_ratio'][1] = [-4.0, -1.3] # logq limits
+fit_2.fit_parameters['alpha'][1] = [-3.14, 3.14] # alpha limits (in radians)
+fit_2.fit_parameters['piEN'][1] = [-0.5, 0.5]
+fit_2.fit_parameters['piEE'][1] = [-0.5, 0.5]
+
+### Allow multiprocessing
 import multiprocessing as mul
 pool = mul.Pool(processes = 4)
 
-### Perform the fit
-my_fit = DE_fit.DEfit(fspl, telescopes_fluxes_method='polyfit', DE_population_size=10, max_iteration=10000, display_progress=True)
-my_fit.fit(computational_pool = pool)
+### !!! WARNING !!!: By executing the next commands you will start a long 
+### search of the parameter space. This is how you would do it in practice but since it 
+### takes a long time, we recommend you skip this step by leaving perform_long_fit = False
+### and using the precomputed optimized parameters given below.
 
-##############################
+perform_long_fit = False
 
+### Fit the model:
+if perform_long_fit == True:
+    fit_2.fit(computational_pool = pool)
+    
+    # Save it
+    np.save('results_USBL_DE_966.npy', fit_2.fit_results['DE_population'])
 
-### looks to be some residuals on the event wings, maybe parallax.
-### Lets try, you have to choose topar, the parallax time reference. Here we choose 2457850.
-model_2 = microlmodels.create_model('PSPL', your_event,parallax=['Full',2457850])
+else:
+    # Use the precomputed Differential Evolution (DE) results:
+    fit_2.fit_results['DE_population'] = np.load('results_USBL_DE_966.npy')
+    fit_2.fit_results['best_model'] = fit_2.fit_results['DE_population'][346501][0:-1]
+    #fit_2.fit_results['best_model'] = [2457205.21, 0.0109583755, 1.78218726, -2.89415218, 0.0475121003, -3.79996021, 2.25499875, 0.0227712230, -0.227192561]
 
-# we can speed up computation, by adding guess from last fit :
-
-#################### guess =   [to,uo,tE]                                + [piEN,piEE]  
-model_2.parameters_guess = your_event.fits[0].fit_results[:3]+[0,0]
-
-your_event.fit(model_2,'LM')
-
-### Let's see some plots.
-your_event.fits[-1].produce_outputs()
-print ('Chi2_LM :',your_event.fits[-1].outputs.fit_parameters.chichi)
+# Plot the best fit model and the corresponding geometrical configuration
+pyLIMA_plots.list_of_fake_telescopes = []
+pyLIMA_plots.plot_lightcurves(usbl, fit_2.fit_results['best_model'])
+pyLIMA_plots.plot_geometry(usbl, fit_2.fit_results['best_model'])
 plt.show()
 
-
-### That looks better! We can check with DE!
-your_event.fit(model_2,'DE',DE_population_size=5)
-
-### Let's see some plots.
-your_event.fits[-1].produce_outputs()
-print ('Chi2_LM :',your_event.fits[-1].outputs.fit_parameters.chichi)
-plt.show()
-
-
-### What about Space based parallax? Lets have a look to OB150966 :
-### http://adsabs.harvard.edu/abs/2016ApJ...819...93S
-
-
-
-your_event = event.Event()
-your_event.name = 'OB150966'
-
-#Here RA DEC matters !!
-your_event.ra = 268.75425
-
-your_event.dec = -29.047
-
-
-## Now we need some observations. That's good, we obtain some data on two
-### telescopes. Both are in I band and magnitude units :
-
-data_1 = np.loadtxt('./data/OGLE_OB150966.dat')
-telescope_1 = telescopes.Telescope(name='OGLE', camera_filter='I', light_curve_magnitude=data_1)
-
-data_2 = np.loadtxt('./data/SPITZER_OB150966.dat')
-telescope_2 = telescopes.Telescope(name='SPITZER', camera_filter='IRAC1', light_curve_magnitude=data_2)
-telescope_2.location='Space'
-telescope_2.spacecraft_name  = 'Spitzer'
-### Add the telescopes to your event :
-your_event.telescopes.append(telescope_1)
-your_event.telescopes.append(telescope_2)
-
-
-
-### Sanity check
-your_event.check_event()
-
-### Construct the model you want to fit. Let's go basic with a PSPL, without second_order effects :
-model_1 = microlmodels.create_model('PSPL', your_event)
-
-
-### Let's try with the simplest Levenvberg_Marquardt algorithm :
-your_event.fit(model_1,'LM')
-
-
-### Let's see some plots.
-your_event.fits[-1].produce_outputs()
-print ('Chi2_LM :',your_event.fits[-1].outputs.fit_parameters.chichi)
-plt.show()
-
-### Of course, not great at all! :
-
-### Construct the model with parallax centered at 2457205:
-model_2 = microlmodels.create_model('PSPL', your_event,parallax=['Full',2457205])
-your_event.fit(model_2,'DE')
-
-
-### Let's see some plots.
-your_event.fits[-1].produce_outputs()
-print ('Chi2_LM :',your_event.fits[-1].outputs.fit_parameters.chichi)
-plt.show()
-
-# Street et al. found piE =(0.0234, -0.238), close to your fit in principle.
+# This solution is close to the (+,+),wide solution reported in 
+# https://ui.adsabs.harvard.edu/abs/2016ApJ...819...93S/
+# (Table 1, Col 2 in the paper - with the units converted to our format):
+#published_model_1 = [2457205.198, 0.0114, 1.76, -2.853, 0.0473, -3.78, 2.26, 0.0234, -0.238]
 
 ### This concludes tutorial 5.
 
