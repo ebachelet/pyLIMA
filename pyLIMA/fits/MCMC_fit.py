1ceae9c2c77412ac25737f27bad6898849efd153
ebachelet <etibachelet@gmail.com>
2023-02-27 20:24:54 +0100

    Fix MCM

--- pyLIMA/fits/MCMC_fit.py
+++ pyLIMA/fits/MCMC_fit.py
@@ -100,8 +100,8 @@ class MCMCfit(MLfit):
             nlinks = self.MCMC_links
 
             # Initialize the population of MCMC
-            eps = 10**-2
-            floors = np.floor(best_solution)
+            eps = 10**-4
+            floors = np.floor(np.round(best_solution))
             initial = best_solution-floors
             deltas = initial*np.random.uniform(-eps,eps,(nwalkers,number_of_parameters))
 
