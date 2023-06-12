9daee36b2cd8df9cefa3a1b1faba56f07d317c80
ebachelet <etibachelet@gmail.com>
2023-06-12 16:43:53 -0700

    Reformat to 88 column**15

--- pyLIMA/models/__init__.py
+++ pyLIMA/models/__init__.py
@@ -6,3 +6,6 @@ from .FSPLarge_model import FSPLargemodel
 from .PSBL_model import PSBLmodel
 from .PSPL_model import PSPLmodel
 from .USBL_model import USBLmodel
+
+__all__ = ["DFSPLmodel","DSPLmodel","FSBLmodel","FSPLmodel","FSPLargemodel",
+           "PSPLmodel","PSBLmodel","USBLmodel"]
