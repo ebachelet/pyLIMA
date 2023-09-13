Fix soft_l1 gradient**2

pyLIMA/fits/ML_fit.py
@@ -849,7 +849,8 @@ class MLfit(object):
             residuals = np.concatenate(residuals)
             errors = np.concatenate(errors)
 
-            ln_likelihood += np.sum(residuals / errors + np.log(errors) + np.log(2 * np.pi))
+            ln_likelihood += np.sum(residuals / errors + np.log(errors) +
+                                    np.log(2 * np.pi))
 
             ln_likelihood *= 0.5
 

pyLIMA/models/ML_model.py
@@ -850,8 +850,9 @@ class MLmodel(object):
 
         piE = np.array([pyLIMA_parameters.piEN, pyLIMA_parameters.piEE])
 
-        parallax_delta_tau, parallax_delta_beta = pyLIMA.parallax.parallax.compute_parallax_curvature(
-            piE, parallax_delta_positions)
+        parallax_delta_tau, parallax_delta_beta = (
+            pyLIMA.parallax.parallax.compute_parallax_curvature(piE,
+                                                                parallax_delta_positions))
 
         return parallax_delta_tau, parallax_delta_beta
 