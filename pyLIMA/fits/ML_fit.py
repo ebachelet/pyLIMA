Fix soft_l1 gradient

pyLIMA/fits/ML_fit.py
@@ -800,54 +800,60 @@ class MLfit(object):
 
             pyLIMA_parameters = self.model.compute_pyLIMA_parameters(model_parameters)
 
-        if self.rescale_photometry:
-
-            rescaling_photometry_parameters = 10 ** (
-                parameters[self.rescale_photometry_parameters_index])
-
-        else:
-
-            rescaling_photometry_parameters = None
-
-        if self.rescale_astrometry:
-
-            rescaling_astrometry_parameters = 10 ** (
-                parameters[self.rescale_astrometry_parameters_index])
-
-        else:
-
-            rescaling_astrometry_parameters = None
-
-        residus, err = self.model_residuals(pyLIMA_parameters,
-                                            rescaling_photometry_parameters=rescaling_photometry_parameters,
-                                            rescaling_astrometry_parameters=rescaling_astrometry_parameters)
-
-        residuals = []
-        errors = []
-
-        for data_type in ['photometry', 'astrometry']:
-
-            try:
-
-                residuals.append(np.concatenate(residus[data_type]) ** 2)
-                errors.append(np.concatenate(err[data_type]) ** 2)
-
-            except ValueError:
-
-                pass
-
-        residuals = np.concatenate(residuals)
-        errors = np.concatenate(errors)
-
-        ln_likelihood = np.sum(residuals / errors + np.log(errors) + np.log(2 * np.pi))
-
         prior = self.get_priors_probability(pyLIMA_parameters)
 
-        ln_likelihood += prior
+        ln_likelihood = prior
 
-        ln_likelihood *= 0.5
+        if np.isinf(ln_likelihood):
 
-        return ln_likelihood, pyLIMA_parameters
+            return ln_likelihood,pyLIMA_parameters
+
+        else:
+
+            if self.rescale_photometry:
+
+                rescaling_photometry_parameters = 10 ** (
+                    parameters[self.rescale_photometry_parameters_index])
+
+            else:
+
+                rescaling_photometry_parameters = None
+
+            if self.rescale_astrometry:
+
+                rescaling_astrometry_parameters = 10 ** (
+                    parameters[self.rescale_astrometry_parameters_index])
+
+            else:
+
+                rescaling_astrometry_parameters = None
+
+            residus, err = self.model_residuals(pyLIMA_parameters,
+                                                rescaling_photometry_parameters=rescaling_photometry_parameters,
+                                                rescaling_astrometry_parameters=rescaling_astrometry_parameters)
+
+            residuals = []
+            errors = []
+
+            for data_type in ['photometry', 'astrometry']:
+
+                try:
+
+                    residuals.append(np.concatenate(residus[data_type]) ** 2)
+                    errors.append(np.concatenate(err[data_type]) ** 2)
+
+                except ValueError:
+
+                    pass
+
+            residuals = np.concatenate(residuals)
+            errors = np.concatenate(errors)
+
+            ln_likelihood += np.sum(residuals / errors + np.log(errors) + np.log(2 * np.pi))
+
+            ln_likelihood *= 0.5
+
+            return ln_likelihood, pyLIMA_parameters
 
     def model_soft_l1(self, parameters):
         """

pyLIMA/models/ML_model.py
@@ -864,9 +864,20 @@ class MLmodel(object):
                                      (1 + pyLIMA_parameters.xi_mass_ratio))
 
         if body !='primary':
-            xallarap_delta_positions *= -1/(pyLIMA_parameters.xi_mass_ratio)
 
+      #      xallarap_delta_positions += pyLIMA.xallarap.xallarap.xallarap_shifts(
+      #          self.xallarap_model,
+      #          time+pyLIMA_parameters.xi_period*pyLIMA_parameters.tE/2,
+      #          pyLIMA_parameters)
 
+             xallarap_delta_positions *= -1/pyLIMA_parameters.xi_mass_ratio
+
+        xallarap_delta_positions -= pyLIMA.xallarap.xallarap.xallarap_shifts(
+                self.xallarap_model,
+                np.array([self.xallarap_model[1]]),
+                pyLIMA_parameters)*(pyLIMA_parameters.xi_mass_ratio /
+                                     (1 + pyLIMA_parameters.xi_mass_ratio))
+        #breakpoint()
         xiE = np.array([pyLIMA_parameters.xiEN, pyLIMA_parameters.xiEE])
 
         xallarap_delta_tau, xallarap_delta_beta = (

pyLIMA/outputs/pyLIMA_plots.py
@@ -61,7 +61,7 @@ def create_telescopes_to_plot_model(microlensing_model, pyLIMA_parameters):
                     model_time2 = np.arange(
                         pyLIMA_parameters.t0 - 1 * pyLIMA_parameters.tE,
                         pyLIMA_parameters.t0 + 1 * pyLIMA_parameters.tE,
-                        1).round(2)
+                        0.01).round(2)
 
                     model_time = np.r_[model_time1, model_time2]
 
@@ -134,7 +134,7 @@ def create_telescopes_to_plot_model(microlensing_model, pyLIMA_parameters):
                     model_time2 = np.arange(
                         pyLIMA_parameters.t0 - 1 * pyLIMA_parameters.tE,
                         pyLIMA_parameters.t0 + 1 * pyLIMA_parameters.tE,
-                        0.1).round(2)
+                        0.01).round(2)
 
                     model_time = np.r_[model_time1, model_time2]
 

pyLIMA/xallarap/xallarap.py
@@ -15,21 +15,19 @@ def xallarap_shifts(xallarap_model, time, pyLIMA_parameters):
                                                       xi_inclination)
 
 
-
     return np.array([separation_1, separation_2])
 
 
 def circular_xallarap(time, t0_xi, xi_period, xi_phase,
                       xi_inclination):
 
-    angular_velocity = 2*np.pi/xi_period#/365.25
 
     angular_velocity = 2*np.pi/xi_period
 
     omega = angular_velocity*(time-t0_xi)+xi_phase
 
-    separation_1 = np.cos(omega)-np.cos(xi_phase)
-    separation_2 = np.sin(xi_inclination)*(np.sin(omega)-np.sin(xi_phase))
+    separation_1 = np.cos(omega)#-np.cos(xi_phase)
+    separation_2 = np.sin(xi_inclination)*(np.sin(omega))#-np.sin(xi_phase))
 
     return separation_1, separation_2
 